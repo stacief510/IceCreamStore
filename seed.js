c66f0ddca0bea351f20a8de089676d017776779e
meghamenon <meg04192012@gmail.com>
2018-04-03 13:58:46 -0700

    created ice cream seed data

---
+++ seed.js
@@ -0,0 +1,40 @@
+var db = require('./models');
+
+var iceCreamFlavors = [
+{
+	flavor: "Vanilla", 
+	allergies: "dairy",
+},
+
+{
+	flavor: "Chocolate",
+	allergies: "dairy"
+	 
+},
+
+{
+	flavor: "Chocolate Chip",
+	allergies: "dairy"
+	
+},
+
+{
+	flavor: "Cookie Dough",
+	allergies: "dairy, egg"
+	
+},
+
+{
+	flavor: "Strawberry",
+	allergies: "dairy, egg"
+	 
+},
+
+{
+	flavor: "Pistachio",
+	allergies: "dairy, nuts"
+	 
+},
+
+
+];
